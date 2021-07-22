070c72716c98f70e4444ae6a3dddad4a3ebedec1
Luís Fernando Gaido <luisfernandogaido@gmail.com>
2021-07-22 15:39:33 -0300

    coisa linda

--- img/img.go
+++ img/img.go
@@ -43,6 +43,7 @@ func Img() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	buider := strings.Builder{}
 	for _, entry := range entries {
 		if entry.IsDir() {
 			continue
@@ -55,9 +56,13 @@ func Img() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		ext := path.Ext(entry.Name())
+		nameIn := strings.TrimSuffix(entry.Name(), ext)
 		contentIn := string(b)
+		if err := os.WriteFile(path.Join(out, entry.Name()), []byte(contentIn), 0644); err != nil {
+			log.Fatal(err)
+		}
 		for _, p := range patterns {
-			ext := path.Ext(entry.Name())
 			nameOut := strings.TrimSuffix(entry.Name(), ext) + "-" + p.Suffix + ext
 			fileNameOut := path.Join(out, nameOut)
 			fmt.Println(nameOut)
@@ -66,5 +71,39 @@ func Img() {
 				log.Fatal(err)
 			}
 		}
+
+		selectors := `button.{{filename}}, a.button.{{filename}} {
+    border-radius: 50%;
+    padding: 0;
+    background-image: url("../img/{{filename}}.svg");
+    background-size: 1.5rem;
+}
+
+button.{{filename}}:hover, a.button.{{filename}}:hover {
+    background-image: url("../img/{{filename}}-hover.svg");
+}
+
+button.{{filename}}:active, a.button.{{filename}}:active {
+    border: 2px solid var(--cor-link-hover);
+    box-shadow: none;
+}
+
+body.alto-contraste button.{{filename}}, body.alto-contraste a.button.{{filename}} {
+    background-image: url("../img/{{filename}}-contraste.svg");
+    background-color: transparent;
+}
+
+body.alto-contraste button.{{filename}}:hover, body.alto-contraste a.button.{{filename}}:hover,
+body.alto-contraste button.{{filename}}:active, body.alto-contraste a.button.{{filename}}:active {
+    border: 2px solid var(--cor-link-contraste);
+}
+
+`
+		selectors = strings.ReplaceAll(selectors, "{{filename}}", nameIn)
+		buider.WriteString(selectors)
+	}
+	cssFile := path.Join(out, "botoes.css")
+	if err := os.WriteFile(cssFile, []byte(buider.String()), 0644); err != nil {
+		log.Fatal(err)
 	}
 }
